3e2893a99327f5472b6dbd4689a9f4ae739741da
xuan1202 <1912425817@qq.com>
2022-05-31 16:13:39 +0800

    test: Hello组件添加h3标签

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -7,6 +7,7 @@ const Hello:React.FC<State>=()=>{
     let [count,setCount] =useState(0)
     return(
         <div>
+            <h3>Hello组件</h3>
             <p>{count}</p>
             <button onClick={()=>setCount(count+2)}>+</button>
         </div>
